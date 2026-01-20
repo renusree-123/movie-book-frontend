f41823f2fed092d2afa235d616ff7faef07f506c
renu <renusreepathipati@gmail.com>
2026-01-20 19:28:45 +0530

    frontend code

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const BASE = import.meta.env.VITE_BASE_URL;
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const fetchMovies = async (query, page = 1) => {
-  const res = await axios.get(`${BASE}/movies?q=${query}&page=${page}`);
+  const res = await axios.get(`${BASE_URL}/movies?q=${query}&page=${page}`);
   return res.data;
 };
 
 export const fetchBooks = async (query, page = 1) => {
-  const res = await axios.get(`${BASE}/books?q=${query}&page=${page}`);
+  const res = await axios.get(`${BASE_URL}/books?q=${query}&page=${page}`);
   return res.data;
 };
